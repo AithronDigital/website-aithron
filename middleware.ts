32a0627c8b054c78fc800e1900c0a0c80290968a
Aithron Digital <mirela25lili@gmail.com>
2026-06-22 19:34:52 +0200

    disable middleware temporarily

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,6 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get('sb-access-token')?.value ||
-    req.cookies.get('sb-vmmkvbmuhyhmeaplwptk-auth-token')?.value
-
-  const isProtected =
-    req.nextUrl.pathname.startsWith('/dashboard') ||
-    req.nextUrl.pathname.startsWith('/admin')
-
-  if (isProtected && !token) {
-    return NextResponse.redirect(new URL('/', req.url))
-  }
-
   return NextResponse.next()
 }
 
